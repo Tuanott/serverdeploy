89aeb59e68c9fb2d7fa171253eeabb18704b5c80
“Tuanott” <patipoltang@gmail.com>
2024-02-28 18:15:46 +0700

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // default port is 3000
+const port = 3000; // default port is 3000
 
 app.get('/', (req, res) => {
     return res.status(200).json({
